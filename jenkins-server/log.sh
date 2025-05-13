68205dff2341035b288b1d582a6c6037bbedb668
Gabrison <100083068+Gabrison@users.noreply.github.com>
2025-05-13 14:21:27 +0300

    added log sample for demonstration

--- jenkins-server/log.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-LOGFILE="/var/log/webz/cluster_monitor.log"
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-RESPONSE=$(curl -s http://172.28.1.100/)
-echo "$TIMESTAMP | $RESPONSE" >> "$LOGFILE"
