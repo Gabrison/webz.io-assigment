2926b4cdc82c16705fdc9a7d73e97cd7bb00c28d
Gabrison <100083068+Gabrison@users.noreply.github.com>
2025-05-13 13:39:05 +0300

    Update configuration files for improved logging and service management
    - Commented out Apache port mapping in docker-compose.yml for clarity.
    - Added logging configuration to corosync.conf to enable logfile output.
    - Modified entrypoint.sh to simplify the Apache homepage message and updated resource configuration commands for clarity.
    - Updated Dockerfile.jenkins to include sshpass for enhanced SSH capabilities.

--- corosync-node/entrypoint.sh
+++ corosync-node/entrypoint.sh
@@ -19,9 +19,9 @@ service apache2 start
 
 # inject message on apache homepage with hostname
 HOMEPAGE="/var/www/html/index.html"
-echo "Junior DevOps Engineer - Home Task from $(hostname)" > "$HOMEPAGE"
+echo "Junior DevOps Engineer - Home Task" > "$HOMEPAGE"
 
-# Only configure the floating IP resource and constraints on webz-001
+# Only configure the floating IP resource and constraints on 1 server: webz-001
 if [ "$(hostname)" = "webz-001" ]; then
   if ! crm status | grep -q ClusterIP; then
     echo "Disabling STONITH in Pacemaker..."
@@ -31,7 +31,7 @@ if [ "$(hostname)" = "webz-001" ]; then
       params ip=172.28.1.100 cidr_netmask=24 \
       op monitor interval=30s
     echo "Setting resource stickiness and location constraints..."
-    crm configure property resource-stickiness=100
+    crm configure rsc_defaults resource-stickiness=100
     crm configure location prefer-webz-001 ClusterIP 100: webz-001
     crm configure location prefer-webz-002 ClusterIP 50: webz-002
     crm configure location prefer-webz-003 ClusterIP 0: webz-003
@@ -39,4 +39,4 @@ if [ "$(hostname)" = "webz-001" ]; then
 fi
 
 # Keep the container running
-tail -F /var/log/syslog 
+tail -F /dev/null 
